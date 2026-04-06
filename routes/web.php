07db97f9ad49e31da31054a099b60405d6abc8b8
dvalverde <doug.valverde.silva@gmail.com>
2026-04-06 17:53:09 -0300

    Ajustado todos os campos para o sign in

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn(Request $request) => $request->user());
+    Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+});
+
+Route::get('/', function () {
+    return view('index');
 });
